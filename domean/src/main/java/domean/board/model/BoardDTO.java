8d16d5ea20f33b3a6bd67822fdd5236fda853f4b
CM <CM@DESKTOP-526L9TK>
2018-09-04 16:31:02 +0900

    테스트2

--- domean/src/main/java/domean/board/model/BoardDTO.java
+++ domean/src/main/java/domean/board/model/BoardDTO.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 public class BoardDTO {
-	 
+	  
 	private String boardSeq;
 	private String boardTitle;
 	private String boardContent;
